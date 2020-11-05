7bc470b482d71f30042e426d49d2eb734c0a1085
Samantha Fok <samantha.fok@hotmail.com>
2020-11-05 07:40:50 +0000

    Completed function to dwnld url file into another file

--- fetcher.js
+++ fetcher.js
@@ -5,12 +5,17 @@ const commUrl = process.argv[2];
 const pathOfFile = process.argv[3];
 
 request(commUrl , (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body.length); // Print the HTML for the Google homepage.
-  console.log(`Downloaded and saved ${body.length} bytes to ${pathOfFile}`)
+  if (error) {
+    console.log('error:', error)
+  }
+  // console.log('response:', response) 
+  // console.log('statusCode:',response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body.length); // Print the HTML for the Google homepage.
+  
+  fs.writeFile(pathOfFile, body, (err) => {
+    if (err) throw err;
+    if (!err) {
+      console.log(`Downloaded and saved ${body.length} bytes to ${pathOfFile}`)
+    }
+  });
 })
-
-fs.writeFile(pathOfFile, { encoding: 'utf8'}, body) => {
-console.log("Copied web data to index.html")
-}
